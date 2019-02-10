b710a22e77c652be4b1c91e6ca51ddf982f53bc1
Matt <mattanator94@gmail.com>
2019-02-10 12:22:08 -0800

    Cached DOM selectors by storing as variables. Added scrollFunction as a window.onload event so that navbar won't display on first page load (unless on a small screen). Made hide and unhide functions more clear with better names. Added comments.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const contactForm = document.getElementById('contact-form');
 const secondaryNav = document.getElementsByClassName('toggle-text');
+const viewportWidth = document.documentElement.clientWidth;
+const documentBodyScroll = document.body;
+const documentElementScroll = document.documentElement;
 
+// Contact form email address removed from main html for some security
 contactForm.setAttribute(
   'action',
   'https:'
@@ -16,29 +20,34 @@ contactForm.setAttribute(
     + 'com',
 );
 
-const hide = () => {
+// Two programs to toggle hidden and unhidden states.
+// classList.toggle would not function here as it is would toggle each time the scroll moved
+const hideText = () => {
   for (let i = 0; i < secondaryNav.length; i++) {
     secondaryNav[i].classList.add('hidden');
   }
 };
-const unHide = () => {
+const unHideText = () => {
   for (let i = 0; i < secondaryNav.length; i++) {
     secondaryNav[i].classList.remove('hidden');
   }
 };
 
+// On scroll event to hide and unhide secondary-nav if the scroll position is greater than 160
+// and screen width is more than 800
 const scrollFunction = () => {
-  if (document.documentElement.clientWidth >= 800) {
-    if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+  if (viewportWidth >= 800) {
+    if (documentBodyScroll.scrollTop > 160 || documentElementScroll.scrollTop > 160) {
       document.getElementById('navbar').style.top = '0';
-      hide();
+      hideText();
     } else {
       document.getElementById('navbar').style.top = '-60px';
-      unHide();
+      unHideText();
     }
   }
 };
 
 window.onscroll = () => scrollFunction();
+window.onload = () => scrollFunction();
 
 console.log('heyyyyyyy');
